d219bcff8ca30e7963492c3fc3ab151d1e5a4beb
Jung Lee <lee.jung.s95@gmail.com>
2021-02-24 00:46:55 +0000

    Automatically backed up by Learn

--- models/pig_latinizer.rb
+++ models/pig_latinizer.rb
@@ -7,7 +7,7 @@ class PigLatinizer
       word = word.gsub("qu", " ") #gsub replaces all occurences with the second argument
       word.gsub!(/^([^aeiou]*)(.*)/,'\2\1ay') # (^)finds the start of the word (^) and grab the subgroup
       word = word.gsub(" ", "qu")
-      
+
     end
     result.join(" ")
   end
