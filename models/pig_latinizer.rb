0ee01c0687b39c18be1ea9fcfe9413783f483354
Jung Lee <lee.jung.s95@gmail.com>
2021-02-24 00:47:33 +0000

    Automatically backed up by Learn

--- models/pig_latinizer.rb
+++ models/pig_latinizer.rb
@@ -4,10 +4,11 @@ class PigLatinizer
     result = sent.split(" ").map do |word|
       if word[0].match(/[AEIOUaeiou]/)
         word + "way"
-      word = word.gsub("qu", " ") #gsub replaces all occurences with the second argument
-      word.gsub!(/^([^aeiou]*)(.*)/,'\2\1ay') # (^)finds the start of the word (^) and grab the subgroup
-      word = word.gsub(" ", "qu")
-
+      else
+        word = word.gsub("qu", " ") #gsub replaces all occurences with the second argument
+        word.gsub!(/^([^aeiou]*)(.*)/,'\2\1ay') # (^)finds the start of the word (^) and grab the subgroup
+        word = word.gsub(" ", "qu")
+      end
     end
     result.join(" ")
   end
