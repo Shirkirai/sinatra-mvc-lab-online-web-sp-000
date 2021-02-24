6b3d1776e3bfc08284fb31125760f790b5e1198f
Jung Lee <lee.jung.s95@gmail.com>
2021-02-24 00:37:16 +0000

    Automatically backed up by Learn

--- models/pig_latinizer.rb
+++ models/pig_latinizer.rb
@@ -2,8 +2,8 @@ class PigLatinizer
 
   def piglatinize(sent)
     result = sent.split(" ").map do |word|
-      word = word.gsub("qu", " ")
-      word.gsub!(/^([^aeiou]*)(.*)/,'\2\1ay')
+      word = word.gsub("qu", " ") #gsub replaces all occurences with the second argument
+      word.gsub!(/^([^aeiou]*)(.*)/,'\2\1ay') #finds the start of the word (^) and grab the subgroup 
       word = word.gsub(" ", "qu")
     end
     result.join(" ")
